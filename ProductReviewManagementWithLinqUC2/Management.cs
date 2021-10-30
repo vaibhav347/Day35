656ef1b846a852fa64038298d111bed6731ac552
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-30 18:46:44 +0530

    File inserted successfully

---
+++ ProductReviewManagementWithLinqUC2/Management.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagementWithLinq
+{
+    class Management
+    {
+        public DataTable dataTable = new DataTable();
+        public void TopRecords(List<ProductReviewMangement> listProductReview)
+        {
+            var recordsData = (from productreviews in listProductReview
+                               orderby productreviews.Rating descending
+                               select productreviews).Take(3);
+
+            foreach (var list in recordsData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            }
+        }
+    }
+}
