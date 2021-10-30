f27002bbbfd32ecd903be584614192b377896a4d
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-30 18:48:30 +0530

    File inserted successfully

---
+++ ProductReviewManagementWithLinqUC4/Management.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagementWithLinq
+{
+    class Management
+    {
+        public DataTable dataTable = new DataTable();
+        public void RetrieveCountOfRecords(List<ProductReviewMangement> listProductReview)
+        {
+            var RecordData = listProductReview.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+            foreach (var list in RecordData)
+            {
+                Console.WriteLine(list.ProductID + "------- " + list.Count);
+            }
+        }
+    }
+}
