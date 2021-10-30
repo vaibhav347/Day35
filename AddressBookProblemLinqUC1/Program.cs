99faa47a375601b2cac3a2982f53a0d10debd0de
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-30 18:55:36 +0530

    File inserted successfully

---
+++ AddressBookProblemLinqUC1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblemLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book problem statement");
+
+            List<AddressBook> address = new List<AddressBook>()
+            {
+                 new AddressBook{Name="Vaibhav",Contact=9359864239,City="Pune",State="Maharashtra",Pin=431115},
+                 new AddressBook{Name="Pratibha",Contact=9359868837,City="Pune",State="Maharashtra",Pin=431116},
+            };
+           foreach(var list in address)
+            {
+            Console.WriteLine("Name:-"+list.Name+" "+"Contact:-"+list.Contact+" "+"City:-"+list.City+" "+"State:-"+list.State+" "+"Pin Code:-"+list.Pin);
+            }
+        }
+    }
+}
